5421ca83ab342e499ac948b4e029a9066c9384eb
mandark216 <107990793+mandark216@users.noreply.github.com>
2022-07-05 12:11:49 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,163 @@
+let start = document.getElementById('mx')
+let quiz = document.getElementById('quiz')
+let timer = document.getElementById('timer')
+let question = document.getElementById('question')
+let counter = document.getElementById('counter')
+let choiceA = document.getElementById('a')
+let choiceB = document.getElementById('b')
+let choiceC = document.getElementById('c')
+
+let progess = document.getElementById("progess")
+
+let scorecontainer = document.getElementById('scorecontainer')
+let scorer = document.getElementById('score')
+
+let questions = [
+    {
+        question: "Who is CM of Maharashtra ?",
+        choiceA: "Devendra Fadnavis",
+        choiceB: "Eknath Shinde",
+        choiceC: "Uddhav Thakre ",
+        correct: "b"
+
+    },
+    {
+        question: "City of Gates ?",
+        choiceA: "Aurangabad",
+        choiceB: "Pune",
+        choiceC: "Mumbai",
+        correct: "a"
+
+    },
+
+    {
+        question: "What was the old name of VJTI ?",
+        choiceA: "Veermata Jijabai Technological Institute",
+        choiceB: "Veerbai Jijamata Technical Institute",
+        choiceC: "Victoria Jublee Technical Institue ",
+        correct: "C"
+
+    },
+    {
+        question: "Richest Area in Mumbai",
+        choiceA: "Malbar hills",
+        choiceB: "Dharavi",
+        choiceC: "Ghatkopar ",
+        correct: "a"
+
+    },
+    {
+        question: "Where we got peace in Mumbai",
+        choiceA: "marine Drive",
+        choiceB: "Dadar market",
+        choiceC: "Malls ",
+        correct: "a"
+
+    }
+
+]
+
+
+let lastquestion = questions.length ;
+let runningquestion = 0;
+
+// render question 
+function renderquestion() {
+    let q = questions[runningquestion];
+
+    question.innerHTML = q.question;
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+
+}
+
+start.addEventListener("click", startquiz);
+
+function startquiz() {
+    start.style.display = "none";
+    quiz.style.display = "flex"
+    renderquestion();
+    TIME = setInterval(updatetime, 1000)
+    Submit();
+    
+}
+
+
+// render progess
+
+
+
+
+// render counter
+
+
+let TIME;
+let count = 0;
+const timeq = 15;
+
+function updatetime() {
+    if (count <= timeq) {
+
+        counter.innerHTML = count;
+        count++;
+    }
+    else {
+        count = 0;
+        if (runningquestion < lastquestion) {
+            runningquestion++;
+            renderquestion()
+        }
+        else{
+            clearInterval(TIME)
+            scorerender();
+        }
+    }
+    
+}
+let score=0;
+
+function checkans(answer) {
+    if (answer === questions[runningquestion].correct) {
+
+        score++;
+        // ans is correct
+    }
+    else {
+        // ans is correct
+        score = 0;
+    }
+    count = 0;
+    if (runningquestion <= lastquestion) {
+        runningquestion++;
+        renderquestion()
+
+    }
+    else{
+        clearInterval(TIME)
+        scorerender();
+
+    }
+
+}
+
+
+function scorerender(){
+
+scorecontainer.style.display="flex"
+scorer.innerHTML="Corrected :"+score;
+document.getElementById('perc').innerHTML="Score : "+Math.round(score*100/questions.length)+"%";
+
+clearInterval(TIME)
+quiz.style.display='none'
+}
+
+function submit(){
+clearInterval(TIME);
+quiz.style.display="none";
+scorecontainer.style.display="flex";
+scorer.innerHTML="Corrected :"+score;
+document.getElementById('perc').innerHTML="Score : "+Math.round(score*100/questions.length)+"%";
+
+
+}
